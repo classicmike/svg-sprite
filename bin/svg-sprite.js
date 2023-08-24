f31c8003297b313aa3d2db29cc1cbb8f6be86f3c
michaeltran <michael.tran@sandstone.com.au>
2023-08-24 17:54:08 +1000

    Changed lodash related to official latest supported package.

--- bin/svg-sprite.js
+++ bin/svg-sprite.js
@@ -16,7 +16,7 @@
  */
 const fs = require('fs');
 const path = require('path');
-const merge = require('lodash.merge');
+const merge = require('lodash').merge;
 const File = require('vinyl');
 const yaml = require('js-yaml');
 const glob = require('glob');
